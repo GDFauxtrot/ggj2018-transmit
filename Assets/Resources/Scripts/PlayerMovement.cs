d123cc063e3267d3b2f623914616c941656505da
Ittaimann <ittaiam@gmail.com>
2018-01-27 11:51:20 -0800

    a couple things I suppose

--- Assets/Resources/Scripts/PlayerMovement.cs
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour {
 		Vector2 movement = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
 
         movement.Normalize();
-		rb2D.MovePosition(rb2D.position+movement*speed);
+		rb2D.MovePosition(rb2D.position+movement*speed*Time.deltaTime);
 		// Mathf.Atan2 returns the tangent line to the two float values given, and then we multiple it to get it as an angle.
         reticle.eulerAngles = new Vector3(0, 0,Mathf.Atan2(Input.GetAxis("RstickVertical"), Input.GetAxis("RstickHorizontal")) * 180 / Mathf.PI);
     } 
---
+++ Assets/Resources/Scripts/bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+	public float speed;
+	// Update is called once per frame
+	void Update () {
+		Vector3 moving=new Vector3(speed * Time.deltaTime,0,0);
+		transform.position+= (moving);
+	}
+}
