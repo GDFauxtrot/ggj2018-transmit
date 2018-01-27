787b0d0e0efb76917f22c84465c4c1bc5bef14bc
gregduarte97@gmail.com <Greg D>
2018-01-27 13:59:37 -0800

    GameManager functions
    Created a bunch of empty functions, going to populate them next
    
    Created an EnemyType enum to organize stuff

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum EnemyType { SHOOTER, BRAWLER, BOSS };
+
 public class GameManager : MonoBehaviour {
 
-    public GameObject player, inGameCamera;
-
-    public GameObject spawnPoints;
+    public GameObject player, inGameCamera, spawnPoints;
+    public GameObject shooterPrefab, brawlerPrefab;
 
     public bool bossSpawned; // To prevent him from spawning again
 
@@ -16,14 +17,14 @@ public class GameManager : MonoBehaviour {
 
     public float spawnPointRange; // How far away can enemies spawn from the spawn points (spawning them exactly on would be boring)
 
-    public float pixelEffectIntensity, pixelEffectTime, pixelEffectFadeTime;
+    public float pixelEffectIntensity, pixelEffectTime, pixelEffectFadeTime, lagEffectTime;
 
     List<GameObject> enemySpawnPoints; // This is all we need, as far as I'm concerned
 
-    InGameCameraManager inGameCamManager;
+    InGameCameraManager inGameCameraManager;
 
     void Start () {
-        inGameCamManager = inGameCamera.GetComponent<InGameCameraManager>();
+        inGameCameraManager = inGameCamera.GetComponent<InGameCameraManager>();
 
         foreach (Transform childTransform in spawnPoints.transform) {
             enemySpawnPoints.Add(childTransform.gameObject);
@@ -36,24 +37,9 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    public void SpawnRaptors(int count, bool dontSpawnTogether = false) {
-        GameObject raptorPrefab = Resources.Load<GameObject>("Prefabs/Raptor");
-        int spawnPointIndex = Random.Range(0, enemySpawnPoints.Count);
-
-        for (int i = 0; i < count; ++i) {
-            if (dontSpawnTogether) // flip this bool to put each raptor at a different spawn point
-                spawnPointIndex = Random.Range(0, enemySpawnPoints.Count);
-
-            Vector2 spawnPos = enemySpawnPoints[spawnPointIndex].transform.position;
-            raptorPrefab.transform.position = new Vector2(
-                spawnPos.x + Random.Range(-spawnPointRange, spawnPointRange),
-                spawnPos.y + Random.Range(-spawnPointRange, spawnPointRange));
-            Instantiate(raptorPrefab);
-        }
-    }
-
     public void SpawnBoss() {
         bossSpawned = true;
+        // and then do the rest
     }
 
     public void SendCommand(string message) {
@@ -64,33 +50,91 @@ public class GameManager : MonoBehaviour {
 
         switch (messageSplit[0]) {
             case "spawn":
-                break; // check for messageSplit[1] -- spawn what?
-            case "summon":
-                break; // same as spawn
+                break;
             case "heal":
-                break; // heal the player
-            case "distort":
+                break;
+            case "pixellate":
                 StartCoroutine(PixelEffect(pixelEffectIntensity, pixelEffectTime, pixelEffectFadeTime));
                 break;
+            case "lag":
+                StartCoroutine(LagEffect(lagEffectTime));
+                break;
         }
+    }
+
+    //                                                                           //
+    // -- GameManager Chat Functions and Coroutines (accessed by SendCommand) -- //
+    //                                                                           //
+
+    // Preferrably unless the different enemy types need different spawn behaviors,
+    // just keep this as generic prefab spawn and pass in the enemy prefab you want
+    public void SpawnEnemy(GameObject prefab, int count, bool dontSpawnTogether = false) {
+        int spawnPointIndex = Random.Range(0, enemySpawnPoints.Count);
+
+        for (int i = 0; i < count; ++i) {
+            if (dontSpawnTogether) // flip this bool to put each one at a different spawn point
+                spawnPointIndex = Random.Range(0, enemySpawnPoints.Count);
+
+            Vector2 spawnPos = enemySpawnPoints[spawnPointIndex].transform.position;
+            prefab.transform.position = new Vector2(
+                spawnPos.x + Random.Range(-spawnPointRange, spawnPointRange),
+                spawnPos.y + Random.Range(-spawnPointRange, spawnPointRange));
+            Instantiate(prefab);
+        }
+    }
+
+    private void SetPlayerHealth(int health) {
+
+    }
+
+    private void SetEnemyHealth(EnemyType type, int health) {
+
+    }
+
+    private void SetPlayerSpeed(int speed) {
+
+    }
+
+    private void SetEnemySpeed(EnemyType type, float speed) {
+
+    }
+
+    private void SetPlayerInvincible(float time) {
+
+    }
+
+    private void UpgradePlayerBullets() {
+
+    }
+
+    private void PlayerDamageMultiplier(float multiplier, float time) {
+
+    }
+
+    private void EnemyDamageMultiplier(EnemyType type, float multiplier, float time) {
+
+    }
+
+    private IEnumerator LagEffect(float time) {
+        yield return new WaitForSeconds(0f); // need this here or else it complains
     }
 
     private IEnumerator PixelEffect(float intensity, float time, float fadeTime) {
         float timeStep = 0f;
 
         while (timeStep < 1f) {
-            inGameCamManager.SetPixelEffectIntensity(Mathf.Lerp(0f, intensity, timeStep));
+            inGameCameraManager.SetPixelEffectIntensity(Mathf.Lerp(0f, intensity, timeStep));
             yield return new WaitForSeconds(Time.deltaTime);
             timeStep += Time.deltaTime * (1f/fadeTime);
         }
         timeStep = 1f;
-        inGameCamManager.SetPixelEffectIntensity(intensity);
+        inGameCameraManager.SetPixelEffectIntensity(intensity);
         yield return new WaitForSeconds(time);
         while (timeStep > 0f) {
-            inGameCamManager.SetPixelEffectIntensity(Mathf.Lerp(0f, intensity, timeStep));
+            inGameCameraManager.SetPixelEffectIntensity(Mathf.Lerp(0f, intensity, timeStep));
             yield return new WaitForSeconds(Time.deltaTime);
             timeStep -= Time.deltaTime * (1f/fadeTime);
         }
-        inGameCamManager.SetPixelEffectIntensity(0);
+        inGameCameraManager.SetPixelEffectIntensity(0f);
     }
 }
