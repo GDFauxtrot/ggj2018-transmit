92b9bfe0aff4485b82efdf850143f03971971f19
gregduarte97@gmail.com <Greg D>
2018-01-27 12:20:57 -0800

    Added SendCommand functionality, implemented Pixel effect to test it

--- Assets/Resources/Scripts/InGameCameraManager.cs
+++ Assets/Resources/Scripts/InGameCameraManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -18,5 +19,9 @@ public class InGameCameraManager : MonoBehaviour {
 
         if (playerToFollow != null)
             transform.position = new Vector3(playerToFollow.transform.position.x, playerToFollow.transform.position.y, transform.position.z);
+    }
+
+    internal void SetPixelEffectIntensity(float intensity) {
+        mipMapBias = intensity;
     }
 }
