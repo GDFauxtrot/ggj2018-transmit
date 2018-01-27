871029feeaeda841e518020c4f67520b333bf757
Ittaimann <ittaiam@gmail.com>
2018-01-27 12:46:46 -0800

    little changes

--- Assets/Resources/Scripts/bullet.cs
+++ Assets/Resources/Scripts/bullet.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class bullet : MonoBehaviour {
 
 	public float speed;
+	private Rigidbody2D rb2d;
 	// Update is called once per frame
 	/// <summary>
 	/// Start is called on the frame when a script is enabled just before
 	/// any of the Update methods is called the first time.
 	/// </summary>
-	void Start()
+	void Awake()
 	{
-       // transform.rotation=(transform.localRotation);
-    }
-	void Update () {
-		Vector3 moving=transform.right*speed*Time.deltaTime;
+		rb2d=GetComponent<Rigidbody2D>();
+        Invoke("bulletBeGone", 3f);
 
-		transform.position+= (moving);
-		Invoke("bulletBeGone",2f);
+        // transform.rotation=(transform.localRotation);
+    }
+	void FixedUpdate () {
+		Vector2 moving=transform.right*speed*Time.deltaTime;
+
+		rb2d.MovePosition(rb2d.position+moving);
 	}
 
 	void bulletBeGone()
