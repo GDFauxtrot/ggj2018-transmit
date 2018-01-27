51ff07c33c5a5e57d10229edb1ba5c0a7b760f28
Ittaimann <ittaiam@gmail.com>
2018-01-26 22:51:55 -0800

    player movement and aiming

---
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	private Rigidbody2D rb2D;
+	public Transform reticle;
+	public float speed;
+
+
+	void Awake()
+	{
+		reticle=this.gameObject.transform.GetChild(0).GetComponent<Transform>();
+		rb2D=gameObject.GetComponent<Rigidbody2D>();
+	} 
+
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		Vector2 movement= new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+
+        movement.Normalize();
+		rb2D.MovePosition(rb2D.position+movement*speed);
+		// Mathf.Atan2 returns the tangent line to the two float values given, and then we multiple it to get it as an angle.
+        reticle.eulerAngles = new Vector3(0, 0,Mathf.Atan2(Input.GetAxis("RstickVertical"), Input.GetAxis("RstickHorizontal")) * 180 / Mathf.PI);
+
+
+    } 
+		
+}
