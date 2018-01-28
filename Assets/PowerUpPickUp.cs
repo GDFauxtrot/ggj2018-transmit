989221172a7be706e62766a9ab4774c589927853
Ittaimann <ittaiam@gmail.com>
2018-01-28 13:48:19 -0800

    special bulelts

---
+++ Assets/PowerUpPickUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpPickUp : MonoBehaviour {
+	public bulletScriptable bullets;
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag=="Player")
+		{
+			bullets.upgraded=true;
+		}
+	}
+}
